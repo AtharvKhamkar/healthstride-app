ddb954c00df9335860f01aebaf0f30155a41561f
atharvkhamkar <khamkaratharv2002@gmail.com>
2026-06-19 00:57:18 +0530

    feat: added the syncing for all other health types

--- lib/core/services/health_service.dart
+++ lib/core/services/health_service.dart
@@ -4,13 +4,28 @@ import 'package:health/health.dart';
 class HealthService {
   final Health health = Health();
 
+  ///Supported health datat types
+  static const supportedHealthDataTypes = [
+    HealthDataType.STEPS,
+    HealthDataType.HEART_RATE,
+    HealthDataType.BLOOD_GLUCOSE,
+    HealthDataType.SLEEP_ASLEEP,
+    HealthDataType.ACTIVE_ENERGY_BURNED,
+    HealthDataType.DISTANCE_DELTA,
+    HealthDataType.WEIGHT,
+    HealthDataType.BLOOD_OXYGEN,
+  ];
+
   Future<List<HealthDataPoint>> getStepData() async {
     try {
       final now = DateTime.now();
-      final from = now.subtract(const Duration(hours: 12));
+      final from = now.subtract(const Duration(hours: 6));
       final granted = await health.requestAuthorization(
-        [HealthDataType.STEPS],
-        permissions: [HealthDataAccess.READ],
+        supportedHealthDataTypes,
+        permissions: List.generate(
+          supportedHealthDataTypes.length,
+          (_) => HealthDataAccess.READ,
+        ),
       );
       if (!granted) {
         throw Exception('Health permission denied');
@@ -19,7 +34,7 @@ class HealthService {
       return await health.getHealthDataFromTypes(
         startTime: from,
         endTime: now,
-        types: [HealthDataType.STEPS],
+        types: supportedHealthDataTypes,
       );
     } catch (e) {
       debugPrint('ERROR :: HealthService.getStepData :: ${e.toString()}');
